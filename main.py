74edde17eeb53c5a0008615db678358777b15286
Davi Alves de Lima <odavi@MacBook-Air-de-Davi.local>
2025-07-30 19:14:01 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from nltk.chat.util import Chat, reflections
+
+reflexoes = {
+    'eu': 'voce',
+    'eu sou': 'voce e',
+    'eu era': 'voce era',
+    'eu iria': 'voce iria',
+    'eu irei': 'voce ira',
+    'meu': 'seu',
+    'voce': 'eu',
+    'voce e': 'eu sou',
+    'voce era': 'eu era',
+    'voce iria': 'eu iria',
+    'seu': 'meu'
+}
+
+print(reflexoes)
